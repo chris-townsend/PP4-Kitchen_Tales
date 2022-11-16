4d455113247c519b738a7423f38b5533a15d3abc
chris-townsend <chris-townsend@live.co.uk>
2022-11-16 10:55:13 +0000

    Add Recipe model to recipes app

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django_extensions.db.fields import AutoSlugField
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Save"), (1, "Publish"))
+
+
+class Recipe(models.Model):
+    """Model for adding recipes to the database"""
+    title = models.CharField(max_length=48, unique=True)
+    slug = AutoSlugField(populate_from='title', unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipe_posts')
+    updated_date = models.DateTimeField(auto_now=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    prep_time = models.CharField(max_length=6, default=0)
+    cook_time = models.CharField(max_length=6)
+    ingredients = models.TextField()
+    method = models.TextField()
+    notes = models.TextField()
+    image = CloudinaryField('image', default='placeholder')
+    status = models.IntegerField(choices=STATUS, default=0)
+    save_recipe = models.ManyToManyField(User, related_name='recipe_likes', blank='True')
+
+    class Meta:
+        ordering = ['-created_date']
+
+    def __str__(self):
+        return f"{self.title}"
+
+    def number_of_saves(self):
+        return self.save_recipe.count()
