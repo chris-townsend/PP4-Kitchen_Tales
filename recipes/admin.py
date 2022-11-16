1c8bfc0a9fd9981b7a285edee13b81baee5da2de
chris-townsend <chris-townsend@live.co.uk>
2022-11-16 12:05:29 +0000

    Add summernote to Recipe model within admin.py

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Recipe
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Recipe)
+
+@admin.register(Recipe)
+class RecipeAdmin(SummernoteModelAdmin):
+    """Admin can manage recipes within the Django admin panel """
+
+    summernote_fields = ('ingredients', 'method', 'notes')
