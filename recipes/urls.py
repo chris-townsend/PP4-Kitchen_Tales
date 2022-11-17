aaeeacd15e6bd806bd251a2fac9056c4e7509d9b
chris-townsend <chris-townsend@live.co.uk>
2022-11-17 13:47:30 +0000

    Close #11 Create all_recipes.html template & display recipe cards

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,10 @@
 from . import views
 from django.urls import path
+from .views import RecipeListHome
 
 """URL Patterns"""
 
 urlpatterns = [
-    path("", views.RecipeList.as_view(), name="home"),
+    path("", views.Home.as_view(), name="home"),
+    path('all_recipes/', views.RecipeListHome.as_view(), name='all_recipes'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,41 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views import generic, View
 from .models import Recipe
 
 
-class RecipeList(generic.ListView):
+class Home(generic.TemplateView):
+    template_name = 'index.html'
+
+
+class RecipeListHome(generic.ListView):
+    """
+    This view is used to display all recipes in the all recipes page
+    """
     model = Recipe
     queryset = Recipe.objects.filter(status=1).order_by('-created_date')
-    template_name = 'index.html'
+    template_name = 'all_recipes.html'
     paginate_by = 8
+
+
+class RecipeDetail(View):
+    """
+    This view is used to display the full recipe details including comments.
+    It also includes the comment form and add to meal plan form
+    """
+    def get(self, request, slug):
+        """
+        Retrives the recipe from the database
+        """
+        queryset = Recipe.objects.all()
+        recipe = get_object_or_404(queryset, slug=slug)
+        bookmarked = False
+        if recipe.bookmarks.filter(id=self.request.user.id).exists():
+            bookmarked = True
+
+        return render(
+            request,
+            "recipe_detail.html",
+            {
+                "recipe": recipe,
+            },
+        )
