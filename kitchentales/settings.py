19e92d9d8e7a66aba05917a8c7ad4015066a64f0
chris-townsend <chris-townsend@live.co.uk>
2022-11-22 12:40:18 +0000

    Add message tags to bootstrap classes in settings.py

--- kitchentales/settings.py
+++ kitchentales/settings.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from django.contrib.messages import constants as messages
 if os.path.isfile('env.py'):
     import env
 
@@ -62,6 +63,15 @@ SITE_ID = 1
 # Redirectional URLS back to the homepage after login/logout
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+# Message Tags set to different Bootstrap classes
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 # Prevents 500 errors during login and registration
 ACCOUNT_EMAIL_VERIFICATION = 'none'
