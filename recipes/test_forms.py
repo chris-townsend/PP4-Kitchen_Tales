db90b007d49429e163e292d6614d622270959490
chris-townsend <chris-townsend@live.co.uk>
2023-01-16 17:38:56 +0000

    Update automated testing for forms in test_forms.py

--- recipes/test_forms.py
+++ recipes/test_forms.py
@@ -16,3 +16,52 @@ class TestForms(TestCase):
         self.assertFalse(form.is_valid())
         self.assertIn('title', form.errors.keys())
         self.assertEqual(form.errors['title'][0], 'This field is required.')
+
+    def test_recipe_description_is_required(self):
+        """
+        Test to ensure recipe description is required
+        """
+        form = RecipeForm(({'description': ''}))
+        self.assertFalse(form.is_valid())
+        self.assertIn('description', form.errors.keys())
+        self.assertEqual(form.errors['description'][0],
+                         'This field is required.')
+
+    def test_recipe_ingredients_is_required(self):
+        """
+        Test to ensure recipe ingredients are required
+        """
+        form = RecipeForm(({'ingredients': ''}))
+        self.assertFalse(form.is_valid())
+        self.assertIn('ingredients', form.errors.keys())
+        self.assertEqual(form.errors['ingredients'][0],
+                         'This field is required.')
+
+    def test_recipe_method_is_required(self):
+        """
+        Test to ensure recipe method is required
+        """
+        form = RecipeForm(({'method': ''}))
+        self.assertFalse(form.is_valid())
+        self.assertIn('method', form.errors.keys())
+        self.assertEqual(form.errors['method'][0],
+                         'This field is required.')
+
+    def test_recipe_notes_is_required(self):
+        """
+        Test to ensure recipe notes are required
+        """
+        form = RecipeForm(({'notes': ''}))
+        self.assertFalse(form.is_valid())
+        self.assertIn('notes', form.errors.keys())
+        self.assertEqual(form.errors['notes'][0],
+                         'This field is required.')
+
+    def test_fields_are_explicit_in_form_metaclass(self):
+        """
+        Test to ensure fields listed in the meta class are present
+        """
+        form = RecipeForm()
+        self.assertEqual(form.Meta.fields, (
+            'title', 'prep_time', 'cook_time', 'description', 'ingredients',
+            'method', 'notes', 'image', 'status'))
