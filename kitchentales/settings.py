bd09121f44cf41783cf93df598d05fcea77d9665
chris-townsend <chris-townsend@live.co.uk>
2023-01-26 23:40:54 +0000

    Remove commented out code from settings.py

--- kitchentales/settings.py
+++ kitchentales/settings.py
@@ -30,11 +30,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-
 DEBUG = False
 
-# Only use this for development
-# DEBUG = 'DEVELOPMENT'
 
 # Allows summernote to be rendered with DEBUG set to False
 X_FRAME_OPTIONS = 'SAMEORIGIN'
